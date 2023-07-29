c28075d20f31ca6828c8732d17c303731014a962
Francislie <mfrancislie@gmail.com>
2023-07-29 07:38:00 -0700

    Login function

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,9 +1,9 @@
 import User from '../model/user.js';
 import bcrypt from 'bcryptjs';
+import { createError } from '../utils/error.js';
 
 export const register = async (req, res, next) => {
   try {
-
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
 
@@ -15,6 +15,26 @@ export const register = async (req, res, next) => {
 
     await newUser.save();
     res.status(200).send('User has been created.');
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, 'User not found!'));
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect)
+      return next(createError(400, 'Wrong password or username!'));
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    // details {} that is what i pass to our client side
+    res.status(200).json({ details: { ...otherDetails }, isAdmin });
   } catch (err) {
     next(err);
   }
